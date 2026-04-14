2d801f94eaa7eb4ac69d2691bce8e050737761af
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2026-04-14 23:49:15 +0530

    fix: Update contract address in main.py, deployed_addresses.json, journal.jsonl, and blockchain.js

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 # 1. CONFIGURATIONS
 # ==========================================
 # Replace this with the address from Step 3!
-CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 # Connect to the local Hardhat Node running in your other terminal
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))

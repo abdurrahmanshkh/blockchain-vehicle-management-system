6300c78948dd1c1e35bc73712d6c018015b35f04
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2026-04-14 18:48:49 +0530

    Reset blockchain

--- blockchain/ignition/modules/VehicleRegistry.js
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("VehicleRegistryModule", (m) => {
-  const vehicleRegistry = m.contract("VehicleRegistry");
-  return { vehicleRegistry };
-});
